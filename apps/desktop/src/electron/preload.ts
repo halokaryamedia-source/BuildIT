eff6243d3cc485722d9278505d2145e006ea9e94
halokaryamedia-source <halokaryamedia@gmail.com>
2026-07-01 20:45:16 +0700

    Remove Electron preload bridge

--- apps/desktop/src/electron/preload.ts
+++
@@ -1,7 +0,0 @@
-import { contextBridge, ipcRenderer } from "electron";
-
-contextBridge.exposeInMainWorld("builditDesktop", {
-  openBlockbench: () => ipcRenderer.invoke("runtime:open-blockbench"),
-  startOllama: () => ipcRenderer.invoke("runtime:start-ollama"),
-  checkRuntime: () => ipcRenderer.invoke("runtime:check")
-});
