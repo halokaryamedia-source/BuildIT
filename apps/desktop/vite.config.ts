503b2dd57bb51f807bb3f7b52f60dc8986bfca15
halokaryamedia-source <halokaryamedia@gmail.com>
2026-07-01 21:09:39 +0700

    Use Svelte Vite plugin

--- apps/desktop/vite.config.ts
+++ apps/desktop/vite.config.ts
@@ -1,9 +1,11 @@
-import react from "@vitejs/plugin-react";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [svelte()],
   server: {
-    port: 5173
+    host: "127.0.0.1",
+    port: 5173,
+    strictPort: true
   }
 });
